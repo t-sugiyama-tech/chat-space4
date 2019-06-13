3e3f1d59cd5927345eae771fb53024b654a193a9
t-sugiyama-tech <new.old.2611@gmail.com>
2019-06-13 15:24:18 +0900

    edit_post_time

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,14 @@ module ChatSpace4
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # -- all .rb files in that directory are automatically loaded. 
+    config.time_zone = 'Tokyo' 
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
-      g.test_framework false
+      g.test_framework false 
     end 
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :ja 
   end
 end
